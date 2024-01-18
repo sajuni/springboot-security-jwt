a7840264e4b0c9094b18e276fd3ee9c920125595
SeungHyo-Lee <sajuni11@naver.com>
2024-01-18 16:53:42 +0900

    에러처리 테스트 중

--- src/main/java/com/example/springbootsecurityjwt/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/springbootsecurityjwt/jwt/JwtTokenProvider.java
@@ -82,19 +82,19 @@ public class JwtTokenProvider {
 
     // 토큰 정보를 검증하는 메서드
     public boolean validateToken(String token) {
-        try {
+//        try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
-        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
-            log.info("Invalid JWT Token", e);
-        } catch (ExpiredJwtException e) {
-            log.info("Expired JWT Token", e);
-        } catch (UnsupportedJwtException e) {
-            log.info("Unsupported JWT Token", e);
-        } catch (IllegalArgumentException e) {
-            log.info("JWT claims string is empty.", e);
-        }
-        return false;
+//        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+//            log.info("Invalid JWT Token", e);
+//        } catch (ExpiredJwtException e) {
+//            log.info("Expired JWT Token", e);
+//        } catch (UnsupportedJwtException e) {
+//            log.info("Unsupported JWT Token", e);
+//        } catch (IllegalArgumentException e) {
+//            log.info("JWT claims string is empty.", e);
+//        }
+//        return false;
     }
 
     private Claims parseClaims(String accessToken) {
